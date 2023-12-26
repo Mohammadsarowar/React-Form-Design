727341f853b8509b7b288404f41a0afca04f1e5b
Md Sarowar <mdsarowarhang@gmail.com>
2023-12-26 05:21:54 +0300

    First commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <p>How to install fuse-react in react.js project.</p>
+        </div>
+    );
+};
+
+export default Sidebar;
